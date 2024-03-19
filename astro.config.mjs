e97d0df91c82678e7e770f09840ebe13b5003676
Jimmi Mortensen <52856973+thatjimmi@users.noreply.github.com>
2024-03-19 15:28:45 +0100

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,35 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
-import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import rehypeExternalLinks from 'rehype-external-links';
+import expressiveCode from 'astro-expressive-code';
+import { expressiveCodeOptions } from './src/site.config';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from '@astrojs/vercel/serverless'
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap(),
-		mdx()
-	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
-})
+  site: 'https://example.me',
+  integrations: [expressiveCode(expressiveCodeOptions), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank',
+      rel: ['nofollow, noopener, noreferrer']
+    }]],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: ['']
+      }
+    }
+  },
+  prefetch: true,
+  output: 'server',
+  adapter: netlify()
+});
--- src/site.config.ts
+++ src/site.config.ts
@@ -3,9 +3,9 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'lorem ipsum',
+	author: 'Jimmi Mortensen',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'lorem',
+	title: 'Jimmi Mortensen',
 	// Meta property used as the default description meta property
 	description: 'The official website of Lorem Ipsum',
 	// HTML lang property, found in src/layouts/Base.astro L:18
